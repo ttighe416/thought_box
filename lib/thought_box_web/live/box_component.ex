4a2f76cb87f82674b2f388e50f837f1f907f18e6
Thomas Tighe <ttighe@sharxplan.com>
2024-01-23 08:22:09 -0600

    Creates LiveComponent for a Box

---
+++ lib/thought_box_web/live/box_component.ex
@@ -0,0 +1,35 @@
+defmodule BoxComponent do
+  use Phoenix.LiveComponent
+  alias ThoughtBox.{ThoughtBox}
+
+
+  def render(assigns) do
+    ~H"""
+    <div class="transition duration-300 shadow my-5 p-3 hover:shadow-2xl flow-root"
+     phx-click="to_box" phx-value-boxid={"#{@box.id}"}>
+      <span> <%= @box.name %> </span>
+      <span class="text-xs">(<%= @box.status %>)</span>
+      <button :if={@box.status == :open }class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded float-right"
+        phx-click="close_box" phx-target={@myself} phx-value-boxid={"#{@box.id}"}>
+        Close Box
+      </button>
+      <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded float-right"
+      phx-click="delete_box" phx-target={@myself} phx-value-boxid={"#{@box.id}"}>
+        Delete Box
+      </button>
+    </div>
+    """
+  end
+
+  def handle_event("close_box", %{"boxid" => box_id} = _params, socket) do
+    {:ok, box} = ThoughtBox.close_box(box_id)
+    send self(), {:update_box, box}
+    {:noreply, socket}
+  end
+
+  def handle_event("delete_box", %{"boxid" => box_id} = _params, socket) do
+    {:ok, box} = ThoughtBox.delete_box(box_id)
+    send self(), {:delete_box, box}
+    {:noreply, socket}
+  end
+end
--- lib/thought_box_web/live/boxes_live.ex
+++ lib/thought_box_web/live/boxes_live.ex
@@ -16,19 +16,9 @@ alias ThoughtBox.{Box, ThoughtBox}
       <.input type="text" autofocus placeholder="Create new box..." field={@form[:name]} />
     </.form>
     <div id="boxes" phx-update="stream">
-      <div :for={{id, box} <- @streams.boxes} id={id}
-        class="transition duration-300 shadow my-5 p-3 hover:shadow-2xl flow-root" phx-click="to_box" phx-value-boxid={"#{box.id}"}>
-          <span> <%= box.name %> </span>
-          <span class="text-xs">(<%= box.status %>)</span>
-          <button :if={box.status == :open }class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded float-right"
-          phx-click="close_box" phx-value-boxid={"#{box.id}"}>
-            Close Box
-          </button>
-          <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded float-right"
-          phx-click="delete_box" phx-value-boxid={"#{box.id}"}>
-            Delete Box
-          </button>
-      </div>
+      <%= for {id, box} <- @streams.boxes do %>
+        <.live_component module={BoxComponent} id={id} box={box}/>
+      <% end %>
     </div>
     """
   end
@@ -49,13 +39,13 @@ alias ThoughtBox.{Box, ThoughtBox}
     {:noreply, push_redirect(socket, to: "/box/#{box_id}")}
   end
 
-  def handle_event("delete_box", %{"boxid" => box_id} = _params, socket) do
-    {:ok, box} = ThoughtBox.delete_box(box_id)
+  def handle_info({:update_box, box}, socket) do
+    {:noreply, stream_insert(socket, :boxes, box)}
+  end
+
+  def handle_info({:delete_box, box}, socket) do
     {:noreply, stream_delete(socket, :boxes, box)}
   end
 
-  def handle_event("close_box", %{"boxid" => box_id} = _params, socket) do
-    {:ok, box} = ThoughtBox.close_box(box_id)
-    {:noreply, stream_insert(socket, :boxes, box)}
-  end
+
 end
